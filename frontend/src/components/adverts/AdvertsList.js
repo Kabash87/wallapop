38a5794a18e158774e9df6279e08383fcf7a4b1a
Ivarsky <iamh90@gmail.com>
2023-08-16 20:38:12 +0000

    build: listado de anuncios

--- frontend/src/components/adverts/AdvertsList.js
+++ frontend/src/components/adverts/AdvertsList.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { getLatestAdverts } from "./service";
+import { Card, CardGroup, ListGroup } from "react-bootstrap";
+import placeholderPhoto from "../../assets/placeholder.png";
 
 const AdvertsList = () => {
   //TODO: dispatch to props
@@ -11,17 +13,41 @@ const AdvertsList = () => {
       setAdverts(adverts);
     });
   }, []);
+  //TODO: el margen izquierdo de las cards desaparece
   return (
     <div className="AdvertsList">
       <h1>Hello ducks!</h1>
-      <ul>
-        {adverts.map((advert) => (
-          <li key={advert.id}>
-            {advert.name}
-            {advert.price}
-          </li>
-        ))}
-      </ul>
+      <div className="d-flex justify-content-center">
+        <CardGroup>
+          {adverts.map((advert) => (
+            <Card
+              key={advert._id}
+              style={{ width: "18rem" }}
+              className="mb-5 mx-2"
+            >
+              <Card.Header>{advert.sell ? "Venta" : "Compra"}</Card.Header>
+              <Card.Img
+                variant="top"
+                src={advert.photo ? advert.photo : placeholderPhoto}
+              />
+              <Card.Body>
+                <Card.Title>{advert.name}</Card.Title>
+                <Card.Text>{advert.description}</Card.Text>
+                <ListGroup variant="flush" bg="primary">
+                  <ListGroup.Item>
+                    {advert.sell
+                      ? `Comprar por: ${advert.price}€`
+                      : `Vender a: ${advert.price}€`}
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Etiquetas: {advert.tags}</small>
+              </Card.Footer>
+            </Card>
+          ))}
+        </CardGroup>
+      </div>
     </div>
   );
 };
